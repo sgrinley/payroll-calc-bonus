91e4f62de91cfac6bdee5aa715db6cffc6a5d3c4
sgrinley <shamar808@yahoo.com>
2026-04-10 09:56:46 -0700

    display: Employee name, job title, & gross pay

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -3,7 +3,28 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class PayrollCalculator {
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your job title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter hours worked: ");
+        float hoursWorked = scanner.nextFloat();
+
+        System.out.print("Enter pay rate: ");
+        float payRate = scanner.nextFloat();
+
+        float grossPay = hoursWorked * payRate;
+
+        System.out.println("Employee Name: " + name);
+        System.out.println("Gross Pay: $" + grossPay);
+
     }
+
 }
